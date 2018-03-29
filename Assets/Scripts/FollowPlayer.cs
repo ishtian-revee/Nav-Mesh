e9e14c451ad63f5925b67db1633e402c9f3b8f99
ishtian revee <ishtian11@gmail.com>
2018-03-29 21:02:50 +0600

    all basic sources added

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform Player;    // A reffernce to the player's transform
+    public Vector3 Offset;      // A variable to store offset position
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Setting the camera position to behind the player forever
+        transform.position = Player.position + Offset;
+
+        // Setting the camera rotation to a little bit down side towards the player
+        transform.rotation = Quaternion.Euler(40, 50, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    // User configurable
+    public Camera Cam;
+    public NavMeshAgent Agent;
+    public ThirdPersonCharacter Character;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Agent.updateRotation = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        // Checks left mouse input
+	    if (Input.GetMouseButtonDown(0))
+	    {
+            // takes mouse input position
+	        Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+
+            // checks if hits any physics collider
+	        if (Physics.Raycast(ray, out hit))
+	        {
+                // sets destination to the hit position
+	            Agent.SetDestination(hit.point);
+	        }
+	    }
+
+        // character movement
+	    if (Agent.remainingDistance > Agent.stoppingDistance)
+	    {
+	        Character.Move(Agent.desiredVelocity, false, true);
+	    }
+	    else
+	    {
+	        Character.Move(Vector3.zero, false, false);
+	    }
+    }
+}
